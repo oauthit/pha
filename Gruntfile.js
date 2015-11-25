575075f5c5b36dbd0b50043b0a57411484c4727c
Albert <kovalevskij.albert@gmail.com>
2015-11-25 17:56:05 +0200

    change ports

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,7 +40,7 @@ module.exports = function (grunt) {
       dev: {
         options: {
           script: 'server/app.js',
-          debug: true
+          debug: false
         }
       },
       prod: {
@@ -109,7 +109,7 @@ module.exports = function (grunt) {
           '<%= yeoman.client %>/assets/images/{,*//*}*.{png,jpg,jpeg,gif,webp,svg}'
         ],
         options: {
-          livereload: true
+          livereload: 17171
         }
       },
       express: {
@@ -118,7 +118,7 @@ module.exports = function (grunt) {
         ],
         tasks: ['express:dev', 'wait'],
         options: {
-          livereload: true,
+          livereload: 17171,
           nospawn: true //Without this option specified express won't be reloaded
         }
       }
